74d6f6365b7b99c991bb5443002e67430c773c2b
rainbowdashx <born_to_kill_666@hotmail.com>
2024-06-09 19:26:04 +0200

    bnk mon updates

--- src/data/enums/bnkmon/bnkmon.ts
+++ src/data/enums/bnkmon/bnkmon.ts
@@ -28,6 +28,8 @@ export const bnkMonLevelMoves:MoveLevelList =   [
   [ 1, Moves.LAST_RESORT ],
   [ 1, Moves.SALT_CURE ],
   [ 1, Moves.ZIPPY_ZAP ],
+  [ 1, Moves.THOUSAND_ARROWS ],
+  [ 1, Moves.BITTER_BLADE ],
   [ 12, Moves.YAWN ],
   [ 16, Moves.BITE ],
   [ 20, Moves.REST ],
